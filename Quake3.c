d56937ddba6ecb3a9053679524311a5fd858c147
Yash-Deodhar <114303252+Yash-Deodhar@users.noreply.github.com>
2024-08-26 17:05:29 -0500

    Add files via upload

---
+++ Quake3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float Q_rsqrt( float number )
+{
+    long i;
+    float x2, y;
+    const float threehalfs = 1.5F;
+
+    x2 = number * 0.5F;
+    y  = number;
+    i  = * ( long * ) &y;    
+    i  = 0x5f3759df - ( i >> 1 );               
+    y  = * ( float * ) &i;
+    y  = y * ( threehalfs - ( x2 * y * y ) );   
+ // y  = y * ( threehalfs - ( x2 * y * y ) );  
+    return y;
+}
+
+int main(int argc, char* argv[])
+{   
+    float res;
+    float N = atof(argv[1]);
+    res = Q_rsqrt(N);
+    printf("%f \n", res);
+    return 0;
+}
