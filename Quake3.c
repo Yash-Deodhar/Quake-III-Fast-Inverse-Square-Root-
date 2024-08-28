2cd354fc189be9403c41d236bc0681cc8b24e1ef
Yash-Deodhar <114303252+Yash-Deodhar@users.noreply.github.com>
2024-08-27 21:30:49 -0500

    Update Quake3.c

--- Quake3.c
+++ Quake3.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float Q_rsqrt( float number )
+// Function to calculate the inverse square root using the "fast inverse square root" method
+float Q_rsqrt(float number)
 {
-    long i;
-    float x2, y;
-    const float threehalfs = 1.5F;
+    long i;                         // Variable to store the bit-level representation of the float
+    float x2, y;                    // Intermediate variables for calculation
+    const float threehalfs = 1.5F;  
+    x2 = number * 0.5F;             
+    y  = number;                    
+    i  = * ( long * ) &y;           // Bitwise cast of y to a long integer
+    i  = 0x5f3759df - ( i >> 1 );   // Perform the magic number operation (bit-level hack)
+    y  = * ( float * ) &i;          // Bitwise cast back to float
+    y  = y * ( threehalfs - ( x2 * y * y ) );  // First iteration of Newton's method
+    // y  = y * ( threehalfs - ( x2 * y * y ) );  // Optionally, a second iteration 
 
-    x2 = number * 0.5F;
-    y  = number;
-    i  = * ( long * ) &y;    
-    i  = 0x5f3759df - ( i >> 1 );               
-    y  = * ( float * ) &i;
-    y  = y * ( threehalfs - ( x2 * y * y ) );   
- // y  = y * ( threehalfs - ( x2 * y * y ) );  
-    return y;
+    return y;                       
 }
 
 int main(int argc, char* argv[])
-{   
-    float res;
-    float N = atof(argv[1]);
-    res = Q_rsqrt(N);
-    printf("%f \n", res);
-    return 0;
+{
+    float res;                      
+    float N = atof(argv[1]);        
+    res = Q_rsqrt(N);               
+    printf("%f \n", res);           
+    return 0;                       
 }
